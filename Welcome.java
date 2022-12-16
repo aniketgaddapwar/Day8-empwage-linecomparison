b38f51d1eb3fa1a0fba1cf8990573f4a8203c936
aniket <aniketgaddapwar@gmail.com>
2022-12-16 23:47:44 +0530

    wage_and_line

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+package com.Day8empwage;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program.....");
+    }
+}
